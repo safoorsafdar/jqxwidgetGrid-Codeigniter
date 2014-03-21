f7daaa836c8ee28df5aeb15a34de4458270fd795
Muhammad Safoor Safdar <safoorsafdar@users.noreply.github.com>
2014-03-21 20:08:06 +0500

    Create index.php

---
+++ application/views/part1/index.php
@@ -0,0 +1,23 @@
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta http-equiv="expires" content="Mon, 10 Dec 2001 00:00:00 GMT" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Customer Detail Listing - jqx Grid-Codiegniter</title>
+    <!-- jqWidgets library css file-->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>theme/jqwidgets/styles/jqx.base.css" charset="UTF-8" />
+ </head>
+ <body>
+<h1>Customer Detail</h1>
+<!-- Div to place grids-->
+<div class="grid" id="allCustomers" ></div>
+<!-- Jquery Library. -->
+<script src="<?php echo base_url(); ?>theme/js/jquery-1.10.2.min.js" charset="UTF-8"></script>
+<!-- jqWidgets library js file-->
+<script src="<?php echo base_url(); ?>theme/jqwidgets/jqx-all.js" charset="UTF-8"></script>
+<script type="text/javascript">var base_url = "<?php  echo base_url(); ?>";</script>
+<script src="<?php echo base_url(); ?>theme/js/views/customers/index.js"></script>
+ </body>
+</html>
